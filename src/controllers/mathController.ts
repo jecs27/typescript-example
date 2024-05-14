cc914303588fe6ed143d4c0c970891261b3dc80e
Jesús Calderón <jesus.calderon@dd3tech.mx>
2024-05-14 10:35:21 -0700

    feat: multiplicationTableWithLimit function added

--- src/controllers/mathController.ts
+++ src/controllers/mathController.ts
@@ -10,3 +10,14 @@ export const multiplicationTable = (req: Request, res: Response) => {
     return res.status(500).send({ status: 500, message: 'Internal Server Error' });
   }
 };
+
+export const multiplicationTableWithLimit = (req: Request, res: Response) => {
+  try {
+    const { number, limit } = req.body;
+    const table = Array.from({ length: limit }, (_v, i) => i + 1).map((i) => i * number);
+    return res.status(200).send({ status: 200, message: '', data: { table } });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({ status: 500, message: 'Internal Server Error' });
+  }
+};
--- src/routes/mathRoute.ts
+++ src/routes/mathRoute.ts
@@ -1,7 +1,12 @@
 import express from 'express';
 
-import { multiplicationTable } from '../controllers/mathController';
+import {
+  multiplicationTable,
+  multiplicationTableWithLimit
+} from '../controllers/mathController';
 
 export const MathRoute = (route: express.Application) => {
   route.post('/math/multiplication-table', multiplicationTable);
+  route.post('/math/multiplication-table-with-limit', multiplicationTableWithLimit);
+
 };
