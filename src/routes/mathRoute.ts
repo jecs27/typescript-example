129dda18ed4d2c83505c5a10be7d5cb0e18d61f1
Jesús Calderón <jesus.calderon@dd3tech.mx>
2024-05-14 22:55:46 -0700

    feat: add multiplicationWithoutMultiplicationOperator function

--- src/routes/mathRoute.ts
+++ src/routes/mathRoute.ts
@@ -2,11 +2,13 @@ import express from 'express';
 
 import {
   multiplicationTable,
-  multiplicationTableWithLimit
+  multiplicationTableWithLimit,
+  multiplicationWithoutMultiplicationOperator
 } from '../controllers/mathController';
 
 export const MathRoute = (route: express.Application) => {
   route.post('/math/multiplication-table', multiplicationTable);
   route.post('/math/multiplication-table-with-limit', multiplicationTableWithLimit);
+  route.post('/math/multiplication-without-multiplication-operator', multiplicationWithoutMultiplicationOperator);
 
 };
